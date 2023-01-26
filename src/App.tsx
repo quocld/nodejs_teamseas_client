cb4deb60739afc8e3fce06fa3e817481b86f322e
quocld <quocle2208@gmail.com>
2023-01-26 16:28:46 +0700

    #feat: subscription totalDonation and Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,78 @@
-import * as React from "react"
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/300.css";
+
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Heading,
+  Text,
+  extendTheme,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
+import Counter from "./donation/Counter";
+import { useQuery, useSubscription } from "urql";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+const TotalDonationQuery = `
+query Query{
+  totalDonations
+}
+`;
+
+const TotalUpdationQuery = `
+subscription Subscription {
+  totalUpdated {
+    total
+  }
+}
+`;
+
+const handleSubscription = (previous: any, newTotal: any) => {
+  return newTotal?.totalUpdated?.total;
+};
+const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat",
+    body: "Montserrat",
+  },
+});
+
+export const App = () => {
+  const [res] = useSubscription(
+    { query: TotalUpdationQuery },
+    handleSubscription
+  );
+
+  const [{ data, fetching, error }] = useQuery({
+    query: TotalDonationQuery,
+  });
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no!... {error.message}</p>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3} bg={"gray.50"}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="20vmin" pointerEvents="none" />
+            <Heading as="h1" size="xl">
+              JOIN THE MOVEMENT!
+            </Heading>
+            <Text fontWeight="light">
+              We did it! Now let’s keep going. Come back anytime <br /> you feel
+              like removing some trash!
+            </Text>
+            <Heading as="h2" size="4xl" fontWeight="bold">
+              <Counter from={0} to={res.data || data.totalDonations} />
+            </Heading>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+};
---
+++ src/donation/Counter.tsx
@@ -0,0 +1,26 @@
+import { animate } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+
+interface Props {
+  from: number,
+  to: number
+}
+
+const Counter = ({from, to}:Props) => {
+  const nodeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const node = nodeRef.current;
+    if(node){
+      const controls = animate(from , to ,{
+        duration: 1,
+        onUpdate(value){
+          node.textContent = parseInt(value.toFixed(0)).toLocaleString()
+        }
+      })
+      return () => controls.stop()  }
+  },[from, to]);
+  return <div ref={nodeRef} />
+};
+
+export default Counter;
